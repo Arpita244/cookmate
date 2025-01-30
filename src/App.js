d401e7c000ad8b1167b46c0eeed9dd7ff35ae5ab
arpita244 <arpitatiwari8756@gmail.com>
2025-01-30 23:52:29 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Recipe from './pages/Recipe';
+import FavoriteRecipes from './pages/FavoriteRecipes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipe/:id" element={<Recipe />} />
+        <Route path="/favorites" element={<FavoriteRecipes />} />
+      </Routes>
+    </Router>
   );
 }
 
